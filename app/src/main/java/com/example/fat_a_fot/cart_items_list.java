f957b44f05a7951c0ff969eb02ee94b4177f57c9
SaiPrudhviG <38295435+SaiPrudhviG@users.noreply.github.com>
2019-01-27 03:33:21 +0530

    added Circular Edit Text and Search Animation image and added drop down box and added static items

---
+++ app/src/main/java/com/example/fat_a_fot/cart_items_list.java
@@ -0,0 +1,13 @@
+package com.example.fat_a_fot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cart_items_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_items_list);
+    }
+}
---
+++ app/src/main/java/com/example/fat_a_fot/splashscreen.java
@@ -0,0 +1,13 @@
+package com.example.fat_a_fot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
